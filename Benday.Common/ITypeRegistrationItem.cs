5d06870b203fca7a3eeacc74f8375c64487ea084
Benjamin Day <benday@benday.com>
2023-03-16 08:40:24 -0400

    Moving code from original repo

---
+++ Benday.Common/ITypeRegistrationItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Benday.Common
+{
+    public interface ITypeRegistrationItem
+    {
+        Type ImplementationType { get; }
+        ServiceLifetime Lifetime { get; set; }
+        Type ServiceType { get; }
+    }
+}
