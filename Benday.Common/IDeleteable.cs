f16e1c7e7cfeb124d3f911672790dce02652b40c
benday <benday@benday.com>
2023-03-20 07:42:28 -0400

    got basic docfx working

--- Benday.Common/IDeleteable.cs
+++ Benday.Common/IDeleteable.cs
@@ -1,7 +1,14 @@
 ﻿namespace Benday.Common
 {
+    /// <summary>
+    /// Interface to indicate that an object is deletable. This is helpful for
+    /// handling changes to collections of objects during CRUD operations. 
+    /// </summary>
     public interface IDeleteable : IInt32Identity
     {
+        /// <summary>
+        /// Set this to true to indicate that this instance should be deleted.
+        /// </summary>
         bool IsMarkedForDelete { get; set; }
     }
 }
