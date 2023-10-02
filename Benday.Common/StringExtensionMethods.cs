7c08f61b3043ae6eb97c3527fdd15c864af6cf8c
Benjamin Day <benday@benday.com>
2023-10-02 09:39:59 -0400

    added string extension methods

---
+++ Benday.Common/StringExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿namespace Benday.Common
+{
+    public static class StringExtensionMethods
+    {
+        public static string SafeToString(this string? input, string defaultValue = "")
+        {
+            if (input is null)
+            {
+                return defaultValue;
+            }
+            else
+            {
+                return input.ToString();
+            }
+        }
+    }
+}
