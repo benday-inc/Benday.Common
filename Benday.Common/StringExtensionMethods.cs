2298aff94f2c5fcfa697e891f800af294a4cf1fa
benday <benday@benday.com>
2023-10-02 09:54:16 -0400

    Updated documentation and nuget package version

--- Benday.Common/StringExtensionMethods.cs
+++ Benday.Common/StringExtensionMethods.cs
@@ -1,7 +1,16 @@
 ﻿namespace Benday.Common
 {
+    /// <summary>
+    /// Extension methods for string.
+    /// </summary>
     public static class StringExtensionMethods
     {
+        /// <summary>
+        /// This wraps the ToString() method and handles the case where the string is null.  Returns the input string if it is not null, otherwise returns the default value.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="defaultValue">Value to return if null. The default value is empty string.</param>
+        /// <returns></returns>
         public static string SafeToString(this string? input, string defaultValue = "")
         {
             if (input is null)
