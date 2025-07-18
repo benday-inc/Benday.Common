bcb51c73e8c79aa82df15effae3336415215645e
benday <benday@benday.com>
2025-07-18 15:31:58 -0400

    added option for user-supplied failure message to IEnumerable asserts

--- Benday.Common.Testing/CheckExtensions.cs
+++ Benday.Common.Testing/CheckExtensions.cs
@@ -4,16 +4,6 @@ namespace Benday.Common.Testing;
 
 public static class CheckExtensions
 {
-    //public static ICheckAssertion<T?> CheckThat<T?>(this T? input)
-    //{
-    //    return new CheckAssertion<T?>(input);
-    //}
-
-    //public static ICheckAssertion<T> CheckThat<T>(this T input) where T : notnull
-    //{
-    //    return new CheckAssertion<T>(input);
-    //}
-
     public static ICheckAssertion<T> CheckThat<T>(this T input) where T : notnull
     {
         return new CheckAssertion<T>(input);
@@ -32,7 +22,6 @@ public static class CheckExtensions
 
     public static ICheckArrayAssertion<T[]> CheckThat<T>(this T[] input)
     {
-        // specialized array handling
         return new CheckArrayAssertion<T[]>(input);
     }
 
